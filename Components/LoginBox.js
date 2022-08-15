aee7c3a9e60fba408ae7929709aa1685340d2da0
Divyanshu Kunwar <kdivyanshu3@gmail.com>
2022-08-15 20:36:54 +0530

    login page design in process

--- Components/LoginBox.js
+++ Components/LoginBox.js
@@ -1,4 +1,8 @@
+import Image from 'next/image';
+
+import googleIcon from '../Images/googleIcon.svg'
 import style from './LoginBox.module.css';
+
 export default function LoginBox() {
     return (
         <div>
@@ -7,9 +11,21 @@ export default function LoginBox() {
                     <span className={style.header}>Welcome Back</span>
                     <span className={style.subHeader}>We are excited to see you again</span>
                 </div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div className={style.phoneInputContainer}>
+                    <span className={style.phoneLabel}>Phone</span>
+                    <input type="phone" />
+                </div>
+                
+                <div className={style.OTPContainer}>
+
+                </div>
+                <div className={style.OTPButton}>
+                    Send OTP
+                </div>
+                <div vlassName={style.signInWithGoogle}>
+                    <Image src={googleIcon}  alt=""/>
+                    <span>Sign in with Google</span>
+                 </div>
                 <div></div>
                 <div></div>
             </div>
