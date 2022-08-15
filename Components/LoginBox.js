f9a1e90540bb280889745cf2e6a4d87f56096a1e
Divyanshu Kunwar <kdivyanshu3@gmail.com>
2022-08-15 16:13:30 +0530

    half registration screeen

---
+++ Components/LoginBox.js
@@ -0,0 +1,18 @@
+import style from './LoginBox.module.css';
+export default function LoginBox() {
+    return (
+        <div>
+            <div className={style.loginBox}>
+                <div>
+                    <span className={style.header}>Welcome Back</span>
+                    <span className={style.subHeader}>We are excited to see you again</span>
+                </div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+}
--- pages/authentication/index.js
+++ pages/authentication/index.js
@@ -1,5 +1,40 @@
+import Lottie from 'react-lottie';
+
+import LoginBox from '../../Components/LoginBox';
+import style from './index.module.css';
+import Logo from '../../components/Logo';
+
+
 function AuthenticationPage(){
-    return <h1>Authentication Page</h1>
+    return <div className={style.authPage}>
+        <div className={style.authTop}>
+            <div>
+                <Logo width="100"  height="50" removeSubText/>
+            </div>
+            <div>
+                About Us
+            </div>
+        </div>
+        <div className={style.authMid}>
+            <div className={style.half}>
+                <Lottie options={{
+                    autoplay: true,
+                    loop: true,
+                    animationData: require('../../images/authentication.json')
+                }}
+                width="70vh"
+                />
+            </div>
+            <div className={style.half}>
+
+            <LoginBox />
+
+            </div>
+        </div>
+        <div className={style.authBottom}>
+            Copyright Blaze @2022
+        </div>
+    </div>
 }
 
 export default AuthenticationPage;
